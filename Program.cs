204d0efb9fc2deb6e419872cd6d3534c38188c82
Daniel <daniyus05@gmail.com>
2023-04-12 13:34:41 +0200

    hola mundo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("¡Hola!, ¿Cómo te llamas?");
-var nombre = Console.ReadLine();
-Console.WriteLine("Mucho gusto, "  + nombre + " ¡Bienvenido!");
+Console.WriteLine("Hola mundo!");
