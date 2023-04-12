500087c34b2997251cda807b1bb523f78f34d310
Daniel <daniyus05@gmail.com>
2023-04-12 13:28:49 +0200

    variable nombre

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("¡Hola mundo!");
+Console.WriteLine("¡Hola!, ¿Cómo te llamas?");
+var nombre = Console.ReadLine();
+Console.WriteLine("Mucho gusto, "  + nombre + " ¡Bienvenido!");
